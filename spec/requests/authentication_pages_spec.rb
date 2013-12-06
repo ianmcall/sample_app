2b26ca0dc09a5ee2ea0c107857b5cc033a8450cc
Ian McAllister <ianmcall@gmail.com>
2013-12-06 14:02:54 -0800

    Authenticate users who access the Settings page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
+require 'utilities'
 
 describe "AuthenticationPages" do
   describe "Authentication" do
 
     subject { page }
+
+    describe "authorization" do
+
+      describe "for non-signed-in users" do
+        let(:user) { FactoryGirl.create(:user) }
+
+        describe "in the users controller" do
+
+          describe "visiting the edit page" do
+            before { visit edit_user_path(user) }
+            it { should have_title('Sign in') }
+          end
+
+          describe "submitting to the update action" do
+            before { patch user_path(user) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+      end
+    end
 
     describe "signin page" do
       before { visit signin_path }
@@ -19,25 +40,22 @@ describe "AuthenticationPages" do
         before { click_button "Sign in" }
 
         it { should have_title('Sign in') }
-        it { should have_selector('div.alert.alert-error', text: "Invalid") }
+        it { should have_error_message('Invalid') }
 
         describe "after visiting another page" do
           before { click_link "Home" }
 
-          it { should_not have_selector('div.alert.alert-error') }
+          it { should_not have_error_message('Invalid') }
         end
       end
 
       describe "with valid information" do
         let(:user) { FactoryGirl.create(:user) }
-        before do
-          fill_in "Email",    with: user.email.upcase
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
+        before { sign_in user }
 
         it { should have_title(user.name) }
         it { should have_link('Profile',    href: user_path(user)) }
+        it { should have_link('Settings',   href: edit_user_path(user)) }
         it { should have_link('Sign out',   href: signout_path) }
         it { should_not have_link('Sign in',  href: signin_path) }
 
